facd30a76621c20b26831050903ab13a5928eddb
Igor Datsenko <Igor.datsenko@hotmail.com>
2018-01-31 14:15:58 +0200

    adds e2e for form;

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,9 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
+
+function selectOption() {
+
+}
 
 describe('angular-test App', () => {
   let page: AppPage;
@@ -7,8 +12,61 @@ describe('angular-test App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   browser.sleep(10000);
+  //   // expect(page.getParagraphText()).toEqual('Welcome to app!');
+  // });
+
+  it('should have form', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(element(by.id('form-title')).getText()).toEqual('Form');
   });
+
+  it('should add value', () => {
+    const el1 = element(by.css('input[name="name"]'));
+    expect(el1).toBeDefined();
+    el1.sendKeys('Ivan');
+    browser.sleep(2000);
+  });
+
+  it('should defined radiobuttons', () => {
+    const el2 = element(by.id('Girl-input'));
+    expect(el2).toBeDefined();
+
+    element(by.id('gender')).all(by.className('mat-radio-label')).get(0).click();
+    browser.sleep(2000);
+  });
+
+  it('should define select age', () => {
+    const el = element(by.id('age'));
+    expect(el).toBeDefined();
+
+    // element(by.name('age')).click();
+    browser.sleep(2000);
+    element(by.id('age'))
+      .all(by.tagName('option'))
+      .get(0)
+      .click();
+    browser.sleep(5000);
+  });
+
+  it('should have datapicker', () => {
+    const el = element(by.id('datapicker'));
+    expect(el).toBeDefined();
+
+    el.all(by.className('btnpicker btnpickerenabled')).get(0).click();
+    browser.sleep(1000);
+
+    element(by.tagName('my-date-picker')).all(by.className('daycell')).get(0).click();
+    browser.sleep(3000);
+
+  });
+
+  it('should enable button submit', () => {
+    const button = element(by.id('submit-button'));
+    expect(button).toBeDefined();
+
+    expect(button.isEnabled()).toBe(true);
+  })
 });
